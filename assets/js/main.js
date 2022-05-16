744d69d1d714a6a4779c78aa4a30c34432b408fd
Sri Balaji <developer.sribalaji@gmail.com>
2022-05-16 13:24:20 +0530

    Magazine - Header strip update

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+  // header strip Date
+  var d = new Date();
+  var month = d.getMonth()+1;
+  var day = d.getDate();
+  var outputdate = ((''+day).length<2 ? '0' : '') + day + '/' + ((''+month).length<2 ? '0' : '') + month + '/' + d.getFullYear();
+    $('#todaysDate').html(outputdate); 
+});
