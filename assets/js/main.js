bc7ae89cb06d81724d0043b76090a62aae229d15
Sri Balaji <developer.sribalaji@gmail.com>
2022-05-17 16:48:41 +0530

    Magazine - Header Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,23 @@
 $( document ).ready(function() {
-  // header strip Date
+  // header branding Date
   var d = new Date();
   var month = d.getMonth()+1;
   var day = d.getDate();
   var outputdate = ((''+day).length<2 ? '0' : '') + day + '/' + ((''+month).length<2 ? '0' : '') + month + '/' + d.getFullYear();
     $('#todaysDate').html(outputdate); 
+  // header branding time
+  "use strict";
+  let targetId = $("#todaysTime");
+  let targetWidth = 0.8;
+  function updateClock() {
+    let time = new Date();
+    let hours = (time.getHours() < 10 ? "0" : "" ) + time.getHours() + "<span class='pointColon'>:</span>";
+    let minutes = (time.getMinutes() < 10 ? "0" : "" ) + time.getMinutes() + "<span class='pointColon'>:</span>";
+    let seconds = (time.getSeconds() < 10 ? "0" : "" ) + time.getSeconds();
+    targetId.html(hours + minutes + seconds);
+    setTimeout(updateClock, 1000);
+  }
+  updateClock();
   //Header article carousel
   $(".header-article").owlCarousel({
     loop: true,
